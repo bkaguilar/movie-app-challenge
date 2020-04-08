deb7a0b2a3ee4978b70185fabd5d601041df0ae7
Bessy Aguilar <bessy.kga@gmail.com>
2020-04-08 20:03:34 +0200

    Add api url

--- my-code/src/constants.tsx
+++ my-code/src/constants.tsx
@@ -1,1 +1,5 @@
-export const API_KEY = 'apikey=79db5715&s';
+export const API_URL = 'http://www.omdbapi.com/';
+export const API_KEY = '&apikey=79db5715&s';
+
+export const DEFAULT_POSTER_PLACEHOLDER =
+  'https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png';
